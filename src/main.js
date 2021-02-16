557897a6cdb4dbf453f570b4208529af72ff847d
Sabrina Li <sabrinali0312@gmail.com>
2021-02-16 09:56:51 -0500

    test fullstory

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as FullStory from '@fullstory/browser';
+
+FullStory.init({ 
+  orgId: '56EM',
+  host: "staging.fullstory.com"
+});
 
 Vue.config.productionTip = false
+Vue.prototype.$FullStory = FullStory
 
 new Vue({
   render: h => h(App),
